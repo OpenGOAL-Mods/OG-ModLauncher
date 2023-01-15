93392a0b91a06346d7ed30cd7c996a192ab33caf
dallmeyer <2515356+dallmeyer@users.noreply.github.com>
2023-01-15 15:49:46 -0800

    add release_data field to json

--- openGOALModLauncher.py
+++ openGOALModLauncher.py
@@ -187,10 +187,10 @@ LATEST_TABLE_SORT = [6, True] # wakeup sorted by last launch date
 
 def getRefreshedTableData(sort_col_idx):
     # uncomment/comment the next two lines if you want to test with a local file
-    mod_dict = requests.get(
-        "https://raw.githubusercontent.com/OpenGOAL-Unofficial-Mods/OpenGoal-ModLauncher-dev/overhaul/resources/mods.json"
-    ).json()
-    # mod_dict = json.loads(open("resources/mods.json", "r").read())
+    # mod_dict = requests.get("https://raw.githubusercontent.com/OpenGOAL-Unofficial-Mods/OpenGoal-ModLauncher-dev/overhaul/resources/mods.json").json()
+    mod_dict = json.loads(open("resources/mods.json", "r").read())
+    mod_dict = dict(sorted(mod_dict.items(), key=lambda x: x[1]["release_date"], reverse=True))
+
     mod_table_data = []
     installed_mod_subfolders = {
         f.name: f.stat().st_mtime for f in os.scandir(ModFolderPATH) if f.is_dir()
