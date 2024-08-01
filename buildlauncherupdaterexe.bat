e52349804197fd5fec47f317e6c3f746a4ba22cd
dallmeyer <2515356+dallmeyer@users.noreply.github.com>
2024-08-01 13:02:51 -0700

    fix autoupdater build script

--- buildlauncherupdaterexe.bat
+++ buildlauncherupdaterexe.bat
@@ -1,6 +1,6 @@
 
 set mypath=%~dp0
-pyinstaller --onefile "Launcher with autoupdater.py" --icon resources\appicon.ico --noconsole
+pyinstaller --onefile "Launcher with autoupdater.py" --icon resources\appicon.ico
 move "%mypath%dist\Launcher with autoupdater.exe" "%mypath%/"
 @RD /S /Q "%mypath%/build"
 @RD /S /Q "%mypath%/dist"
